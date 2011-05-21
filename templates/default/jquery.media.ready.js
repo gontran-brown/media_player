b14ed32beb226b09f79d83c7534f32f9b25d56c7
Jérémy CARLIER <jeremy.carlier@pleinnet.fr>
2011-05-21 21:26:34 +0200

    Quelques nouveautés dans le js

--- templates/default/jquery.media.ready.js
+++ templates/default/jquery.media.ready.js
@@ -1,39 +1,7 @@
-var time = $("#player_mediacurrenttime").text().trim();
-
-   $(document).ready(function() {
-                               var player = $("#player").mediaplayer({
-                                       showPlaylist:false,
-                                       file:"../videos/5_SG.flv",
-                                       image:"videos/urbilog.png",
-                                       prefix:"player_",
-                                       ids: {
-                                               loading:"#player_mediaplayerloading",
-                                               player:"#player_mediaplayer",
-                                               titleBar:"#player_mediatitlebar",
-                                               node:"#player_medianode",
-                                               currentTime:"#player_mediacurrenttime",
-                                               totalTime:"#player_mediatotaltime",
-                                               playPause:"#player_mediaplaypause",
-                                               seekUpdate:"#player_mediaseekupdate",
-                                               seekProgress:"#player_mediaseekprogress",
-                                               seekBar:"#player_mediaseekbar",
-                                               seekHandle:"#player_mediaseekhandle",
-                                               volumeUpdate:"#player_mediavolumeupdate",
-                                               volumeBar:"#player_mediavolumebar",
-                                               volumeHandle:"#player_mediavolumehandle",
-                                               mute:"#player_mediamute",
-                                               busy:"#player_mediabusy",
-                                               preview:"#player_mediapreview",
-                                               play:"#player_mediaplay",
-                                               media:"#player_mediadisplay",
-                                               control:"#player_mediacontrol",
-                                               mediaRegion:"#player_mediaregion"
-                                       }
-                               });
-    
+$(document).ready(function() {
 	//~ var content = $('#mon_tableau_de_donnee');
-	
-	 //~  function submitForm(){  
+
+	 //~  function submitForm(){
 		 //~ var xml;
 		 //~ try{
 			 //~ xml = new ActiveXObject('Msxml2.XMLHTTP');
@@ -50,71 +18,98 @@ var time = $("#player_mediacurrenttime").text().trim();
 				//~ }
 			//~ }
 			//~ }
-			//~ 
+			//~
 			//~ xml.onreadystatechange  = function(){
 				//~ if(xml.readyState  == 4){
 					//~ if(xml.status  == 200)
-					  //~ document.formulaire.mon_tableau_de_donnee="Received:"  + xml.responseText; 
-					//~ else 
+					  //~ document.formulaire.mon_tableau_de_donnee="Received:"  + xml.responseText;
+					//~ else
 					   //~ document.formulaire.mon_tableau_de_donnee="Error code " + xml.status;
 			   //~ }
-		//~ }; 
-	//~ 
-	   //~ xml.open( "GET", "trad.xml",true); 
+		//~ };
+	//~
+	   //~ xml.open( "GET", "trad.xml",true);
 	   //~ consol.log(content);
 	   //~ //xml.send(content);
 	   //~ xml.send(null);
-	   //~ 
+	   //~
 	   //~ var file = $.ajax({
 		   //~ target : '#mon_tableau_de_donnee',
 		   //~ url : 'minima.html',
 			//~ async :false
 		//~ }).responseText;
 		//~ consol.log(file);
-//~ } 
-			
-		  
-   
-   $("#player_loading").hide();
+//~ }
 
-   $("#player").after('<form method =\"POST\" id=\"formulaire\"name=\"formulaire\" action= \"serveur.php\"></form>');
-   $('<INPUT id=\"validation\" type=\"submit\" value=\"Envoyer\"type=\"submit\" >').prependTo('#formulaire');
-  //~ 
-   $('<table name=\"mon_tableau_de_donnee\" id=\"mon_tableau_de_donnee\"></table>').prependTo('#formulaire');
-   var i=0;
+    var $this = $("#player");
+    $this.mediaplayer({
+        showPlaylist:false,
+        file:"../videos/5_SG.flv",
+        image:"videos/urbilog.png",
+        prefix:"player_",
+        ids: {
+            loading:"#player_mediaplayerloading",
+            player:"#player_mediaplayer",
+            titleBar:"#player_mediatitlebar",
+            node:"#player_medianode",
+            currentTime:"#player_mediacurrenttime",
+            totalTime:"#player_mediatotaltime",
+            playPause:"#player_mediaplaypause",
+            seekUpdate:"#player_mediaseekupdate",
+            seekProgress:"#player_mediaseekprogress",
+            seekBar:"#player_mediaseekbar",
+            seekHandle:"#player_mediaseekhandle",
+            volumeUpdate:"#player_mediavolumeupdate",
+            volumeBar:"#player_mediavolumebar",
+            volumeHandle:"#player_mediavolumehandle",
+            mute:"#player_mediamute",
+            busy:"#player_mediabusy",
+            preview:"#player_mediapreview",
+            play:"#player_mediaplay",
+            media:"#player_mediadisplay",
+            control:"#player_mediacontrol",
+            mediaRegion:"#player_mediaregion"
+        }
+    });
 
-   $("#player_mediaplaypause").click(function(){
-       var button = 0;
-       if( i%2 == 0){
-           button = 0;
-       }
-       else{
-           button = 1;
-       }
-       console.log(i);
-       console.log(button);
-       i++;
+    $this.find("#player_loading").hide();
 
-       time = $('#player_mediacurrenttime').text().trim();
+    var i=0;
+    $this.find("#player_mediaplaypause").click(function(){
+        var $form_subtitle = $this.next("form#subtitle");
+        if ($form_subtitle.length == 0){
+            $this.after('<form id="subtitle" method ="POST" action="serveur.php" enctype="multipart/form-data"><input type="submit" value="Envoyer" type="submit"></form>');
+        }
+        else{
+            var button = 0;
 
-       if(button == 0){
-           if (time != null){
-           //**..//    
-               $('<input type =text name=\"debut\" id=\"debut\" value=\"'+time+'\"></input>').prependTo('#mon_tableau_de_donnee');
-                 $('#debut').wrap('<tr id =\"tab'+i+'\">');
-               }
-           }
-       else{
-           console.log('si button different de 0');
-           if (time != null){
-               //~ $('#debut').after($('<input type = \"button\"id=\"valider\"value=\"editer\"></input>'));
-               $('#debut').after($('<input type = text name =\"subtitle\" id=\"subtitle\"></input>'));
-              //**..// 
-                $('#debut').after($('<input type =text name=\"fin\" id =\"fin\"value=\"'+time+'\"></input>'));
-               console.log(time);
-           }
-       }
-       
-   });
-   
+            if (i%2 == 0){
+                button = 0;
+            }
+            else{
+                button = 1;
+            }
+
+            i++;
+            var $time = $this.find("#player_mediacurrenttime").text().trim();
+            console.log($time)
+
+            $form_subtitle_table = $form_subtitle.find("table tbody");
+            if(button == 0){
+                if (($form_subtitle_table.length == 0) && ($time != null)){
+                    $form_subtitle.prepend('<table><tbody><tr><td><input type="text" name="debut'+i+'" value="'+$time+'"></input></td><td></td><td></td><td></td></tr></tbody></table>');
+                }
+                else {
+                    $form_subtitle_table.prepend('<tr><td><input type="text" name="debut'+i+'" value="'+$time+'"></input></td><td></td><td></td><td></td></tr>');
+                }
+            }
+            else{
+                console.log('si button different de 0');
+                if ($time != null){
+                    $('#debut').after($('<input type="text" name="subtitle'+i+'"></input>'));
+                    $('#debut').after($('<input type="text" name="fin'+i+'" value="'+$time+'"></input>'));
+                }
+            }
+        }
+    });
 });
