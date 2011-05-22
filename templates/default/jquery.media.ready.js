444aecec13f9f90e77070da0892d23452d13c112
Soumia ADJIR <adjir.soumia@gmail.com>
2011-05-22 22:09:58 +0200

    Merge different file

--- templates/default/jquery.media.ready.js
+++ templates/default/jquery.media.ready.js
@@ -1,39 +1,3 @@
-<<<<<<< HEAD
-var time = $("#player_mediacurrenttime").text().trim();
-
-   $(document).ready(function() {
-                               var player = $("#player").mediaplayer({
-                                       showPlaylist:false,
-                                       file:"../videos/5_SG.flv",
-                                       image:"videos/urbilog.png",
-                                       prefix:"player_",
-                                       ids: {
-                                               loading:"#player_mediaplayerloading",
-                                               player:"#player_mediaplayer",
-                                               titleBar:"#player_mediatitlebar",
-                                               node:"#player_medianode",
-                                               currentTime:"#player_mediacurrenttime",
-                                               totalTime:"#player_mediatotaltime",
-                                               playPause:"#player_mediaplaypause",
-                                               seekUpdate:"#player_mediaseekupdate",
-                                               seekProgress:"#player_mediaseekprogress",
-                                               seekBar:"#player_mediaseekbar",
-                                               seekHandle:"#player_mediaseekhandle",
-                                               volumeUpdate:"#player_mediavolumeupdate",
-                                               volumeBar:"#player_mediavolumebar",
-                                               volumeHandle:"#player_mediavolumehandle",
-                                               mute:"#player_mediamute",
-                                               busy:"#player_mediabusy",
-                                               preview:"#player_mediapreview",
-                                               play:"#player_mediaplay",
-                                               media:"#player_mediadisplay",
-                                               control:"#player_mediacontrol",
-                                               mediaRegion:"#player_mediaregion"
-                                       }
-                               });		  
-   
-   $("#player_loading").hide();
-=======
 $(document).ready(function() {
 	//~ var content = $('#mon_tableau_de_donnee');
 
@@ -109,7 +73,6 @@ $(document).ready(function() {
     });
 
     $this.find("#player_loading").hide();
->>>>>>> 21d46181a7ecc16fefec1754e8109c2ef24a39c6
 
     var i=0;
     var line=1;
