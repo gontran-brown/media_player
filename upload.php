490b93f4445fbe035cb2a50854f544dff160d474
Soumia ADJIR <adjir.soumia@gmail.com>
2011-05-26 10:19:51 +0200

    rien

--- upload.php
+++ upload.php
@@ -3,7 +3,7 @@ if (isset($_FILES['document'])){
 	$dossier = '../videos/';
 	$fichier = basename($_FILES['document']['name']);
 
-	if(move_uploaded_file($_FILES['document']['tmp_name'],$dossier . $fichier)){
+	if(move_uploaded_file($dossier . $fichier,/*destination*/)){
 		echo'Upload effectué avec succès !';
 	}
 	else{
