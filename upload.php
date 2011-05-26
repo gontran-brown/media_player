2a228d434e17da9e26ac3ab5d19afeb227848abf
Soumia Adjir <adjir.soumia@gmail.com>
2011-05-26 12:32:13 +0200

    un peu de nouveau

--- upload.php
+++ upload.php
@@ -1,14 +1,41 @@
 <?php
 echo 'debut';
-echo "< br/>";
+echo  '<br/>';
+
+/*
+if (isset($_POST['filename'])){
+*/
+		if(!empty($_POST['filename'])){
+			echo 'un filename <br />';
+			echo 'filename =>'.$_POST['filenamee'].'<br/>';
+		}else{
+			 echo 'filename est vide <br />';
+		 }
+/*
+ }
+ else 
+ echo 'il n\'existe pas de filename';
+*/
+
 foreach ($_POST as $key=>$val){
-    echo $key.'=>'.$val;
-    echo 'pour tout les document je veux leur valeur <br />';
-    if ($key == 'document'){
-        echo 'key == document< br/>';
+/*
+	if (isset($_POST['filename'])){
+		if(!empty($_POST['filename'])){
+		echo 'existe un filename <br />';
+	}else{
+		 echo 'filename est vide <br />';
+	 }
+	}
+	
+*/
+    echo $key.'=>'.$val ;
+    echo ' <br /><br />';
+    echo 'pour tout les documents, je veux leur valeur  <br/>';
+    if ($val === 'document'){
+        echo 'key == document  <br/>';
     }
     else
-        echo 'je ne suis pas dans la boucle ou il existe un document< br/>';
+        echo 'je ne suis pas dans la boucle ou il existe un document  <br/>';
 }
 ?>
 
