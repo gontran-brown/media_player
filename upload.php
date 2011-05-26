4b3b37a7c9f389370ad79edb022f9cc4ee95fdac
Soumia Adjir <adjir.soumia@gmail.com>
2011-05-26 10:27:50 +0200

    les php et js

--- upload.php
+++ upload.php
@@ -1,16 +1,26 @@
 <?php
-	echo 'debut';
+	echo 'debut<';
+	echo " br/>";
+	foreach ($_POST as $key=>$val){
+				echo 'pour tout les document je veux leur valeur <br />';
+			if ($key == 'document'){
+				echo 'key == document< br/>';
+			}
+		}
 		if (isset($_POST)){
-			echo 'boucle exist un $_POST';
+			echo 'boucle exist un $_POST< br/>';
+/*
 			foreach ($_POST as $key=>$val){
 				echo 'pour tout les document je veux leur valeur';
 			if ($key == 'document'){
 				echo 'key == document';
 			}
+*/
 
+/*
 			echo 'il exite un docutment';
 			if(!function_exists('downloader')){
-				echo 'dans la boucle !function_exists(\'downloader\')';
+				echo 'dans la boucle !function_exists(\'downloader\')< br/>';
 				function downloader($data, $filename = true, $content = 'application/x-octet-stream'){
 					if(headers_sent()) return false;
 
@@ -21,10 +31,13 @@
 					exit($data);
 				}
 			}
+*/
+/*
 		}  
+*/
 		}
 		else 
-		echo 'je ne suis pas dans la boucle ou il existe un document';
+		echo 'je ne suis pas dans la boucle ou il existe un document< br/>';
 /*
 	}
 */
