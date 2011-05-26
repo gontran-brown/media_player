e245df83d56218a096a5376087632a687dc22c74
Jérémy CARLIER <jeremy.carlier@pleinnet.fr>
2011-05-26 10:44:42 +0200

    Test tableau POST contenu

--- upload.php
+++ upload.php
@@ -1,17 +1,14 @@
 <?php
-	echo 'debut';
-	echo "< br/>";
-	foreach ($_POST as $key=>$val){
-				echo 'pour tout les document je veux leur valeur <br />';
-			if ($key == 'document'){
-				echo 'key == document< br/>';
-			}
-		}
-		if (isset($_POST)){
-			echo 'boucle exist un $_POST< br/>';
-
-		}
-		else 
-		echo 'je ne suis pas dans la boucle ou il existe un document< br/>';
+echo 'debut';
+echo "< br/>";
+foreach ($_POST as $key=>$val){
+    echo $key.'=>'.$val;
+    echo 'pour tout les document je veux leur valeur <br />';
+    if ($key == 'document'){
+        echo 'key == document< br/>';
+    }
+    else
+        echo 'je ne suis pas dans la boucle ou il existe un document< br/>';
+}
 ?>
 
