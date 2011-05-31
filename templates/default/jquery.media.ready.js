3a9f0f521168642509febac79ea3b6738d7185a0
Jérémy CARLIER <jeremy.carlier@pleinnet.fr>
2011-05-31 08:35:54 +0200

    JS corrigé titre des videos clickées

--- templates/default/jquery.media.ready.js
+++ templates/default/jquery.media.ready.js
@@ -57,69 +57,56 @@ $(document).ready(function() {
             volumeHandle:"#mediavolumehandle",
             mute:"#mediamute"
         }
-     });
-     
-	$(window).load(function(){
-		//~ var $this = $("#playerP");
-		var i=0;
-		var line=1;
-		var currentTime=0;
-		var event = jQuery.Event("logged");
-		console.log(event);
-		$this.find("#mediaplaypause").click(function(){
-			console.log();
-			var $form_subtitle = $this.next("form#subtitle");
-			if ($form_subtitle.length == 0){
-				var $help = $this.next("div.help");
-				if ($help.length > 0){
-					$help.remove();
-				}
-				$this.after('<form id="subtitle" method ="POST" action="generate_xml.php" enctype="multipart/form-data">'+
-				'<input type="text" name="filename" value="mes_soustitres"></input>'+
-				'<input  value="Envoyer" type="submit"></form>');
-				$form_subtitle = $this.next("form#subtitle");
-			}
-			var button = i%2;
-			i++;
-			currentTime = $this.find("#mediacurrenttime").text().trim();
-			//console.log("bouton:"+button+";temps:"+currentTime);
-			$form_subtitle_table = $form_subtitle.find("table tbody");
-			if(button == 0){
-				if(($form_subtitle_table.length == 0) && (currentTime != null)){
-					$form_subtitle.prepend('<table><thead><tr><th>Temps début :</th><th>Temps fin :</th><th>Sous titre :</th></tr></thead><tbody><tr><td><input type="text" name="debut'+line+'" value="'+currentTime+'"></input></td><td></td><td></td></tr></tbody></table>');
-				}
-				else{
-					$form_subtitle_table.prepend('<tr><td><input type="text" name="debut'+line+'" value="'+currentTime+'"></input></td><td></td><td></td></tr>');
-				}
-			}
-			else{
-				if(currentTime != null){
-			;		if($form_subtitle_table.length > 0){
-						$form_subtitle_table.find("tr:first-child td:empty:first").append('<input type="text" name="fin'+line+'" value="'+currentTime+'"></input>');
-						$form_subtitle_table.find("tr:first-child td:empty:first").append('<input type="text" name="subtitle'+line+'"></input>');
-						line++;
-					}
-				}
-			}
-		});  
-	//~ console.log(event);
-	//your code here
-	console.log($("#medialist").find("> div"));
-	alert("it's ok");
-	$this.find("#medialist").find("> div").click(function(){	
-			console.log("je viens de cliquer sur une video");
-			//~ console.log($("#medialist").find("> div").find("> div"));
-			//~ console.log($("#medialist").find("> div").find("> div").text().trim());
-			//~ 
-			$this.find("#medialist").each(function(){
-				console.log($("#medialist").find("> div").find("> div").text());
-				//~ if($("#medialist").find("> div").text().trim() =="mediatitle"){
-					//~ console.log("je suis dans le mediatitle");
-				//~ }	
-				//~ else
-				//~ console.log("non");
-			 });
-		});		
-		
-	});
+    });
+
+    $(window).load(function(){
+        //~ var $this = $("#playerP");
+        var i=0;
+        var line=1;
+        var currentTime=0;
+        var event = jQuery.Event("logged");
+        console.log(event);
+        $this.find("#mediaplaypause").click(function(){
+            console.log();
+            var $form_subtitle = $this.next("form#subtitle");
+            if ($form_subtitle.length == 0){
+                var $help = $this.next("div.help");
+                if ($help.length > 0){
+                    $help.remove();
+                }
+                $this.after('<form id="subtitle" method ="POST" action="generate_xml.php" enctype="multipart/form-data">'+
+                            '<input type="text" name="filename" value="mes_soustitres"></input>'+
+                                '<input  value="Envoyer" type="submit"></form>');
+                $form_subtitle = $this.next("form#subtitle");
+            }
+            var button = i%2;
+            i++;
+            currentTime = $this.find("#mediacurrenttime").text().trim();
+            //console.log("bouton:"+button+";temps:"+currentTime);
+            $form_subtitle_table = $form_subtitle.find("table tbody");
+            if(button == 0){
+                if(($form_subtitle_table.length == 0) && (currentTime != null)){
+                    $form_subtitle.prepend('<table><thead><tr><th>Temps début :</th><th>Temps fin :</th><th>Sous titre :</th></tr></thead><tbody><tr><td><input type="text" name="debut'+line+'" value="'+currentTime+'"></input></td><td></td><td></td></tr></tbody></table>');
+                }
+                else{
+                    $form_subtitle_table.prepend('<tr><td><input type="text" name="debut'+line+'" value="'+currentTime+'"></input></td><td></td><td></td></tr>');
+                }
+            }
+            else{
+                if(currentTime != null){
+                    ;		if($form_subtitle_table.length > 0){
+                        $form_subtitle_table.find("tr:first-child td:empty:first").append('<input type="text" name="fin'+line+'" value="'+currentTime+'"></input>');
+                        $form_subtitle_table.find("tr:first-child td:empty:first").append('<input type="text" name="subtitle'+line+'"></input>');
+                        line++;
+                    }
+                }
+            }
+        });
+
+        console.log($("#medialist > div"));
+        alert("it's ok");
+        $this.find("#medialist > div").click(function(){
+            console.log("Voici le titre que vous avez sélectionné : '"+$(this).find("#mediatitle").text().trim()+"'");
+        });
+    });
 });
