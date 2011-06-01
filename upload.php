710ef9aac1cd172be6213588dd9acd407840a961
Jérémy CARLIER <jeremy.carlier@pleinnet.fr>
2011-06-01 10:36:09 +0200

    Premier essaie de manipulatinon d'un XML

--- upload.php
+++ upload.php
@@ -1,49 +1,55 @@
 <?php
- 
-if ($_POST['title'] !=''){
-	echo " <br/>on a un  title <br/>";
-	$var = $_POST['title'];
-echo $var." <br/>";
-	$fp = fopen("playlist.xml", "r"); //lecure du fichier
 
-	while(!feof($fp)){ //parcourt toutes les lignes
-		$page =fgets($fp , 4096); //lecture du contenu de la ligne
-	// echo $page;
-/*
-	$location = eregi("<location>(.*)</location>",,$page ,$reg);
-*/
-/*
-	$titre = eregi("<title>(.*)</title>" ,$page ,$regs);
-*/
-	$location = eregi("<location>(.*)</location>",,$page ,$reg);
-	echo "<br />";
-	//echo $regs[1];
-/*
-	$reg[1] = preg_replace('videos/', '', $reg[1]);
-*/
+if (!empty($_POST['title'])){
+    $currentVideo = $_POST['title'];
+    echo "Notre video : ".$currentVideo."<br />";
 
-	echo $reg[1];
-	if($regs[1] == $var){
-		echo "<br /> on se retrouve dans le if(regs == _POST['title'])<br />";
-		
-		}
-	}
+    $document_xml = new DomDocument;
+    $document_xml->load("playlist.xml") or die ("Fichier introuvable");
+    $listelement = $document_xml->getElementsByTagName("track");
+    foreach ($listelement as $element){
+        // plus d'infos ici si tu ne comprends pas : http://eusebius.developpez.com/php5dom/
+        // autre doc vachement sympas : http://tecfa.unige.ch/guides/tie/pdf/files/php-dom.pdf
+        $title = $element->getElementsByTagName("title");
+        foreach($title as $tt){
+            echo $tt->firstChild->nodeValue;
+        }
+    }
+    /*
+        if ($element->firstChild->nodeValue == $currentVideo){
+            echo "bjbjbj";
+            foreach ($element as $child){
+                echo $child->nodeValue;
+            }
+        }
+        echo $element->firstChild->nodeValue."<br />";
+    }
 
-fclose($fp);
+    // lecture du fichier xml
+    $fp = fopen("playlist.xml", "r") or die ("Fichier introuvable");
+
+    while ($fdata = fread($fp, 2048)){
+        xml_parse($xml_parseur, $fdata, feof($fp)) or die(
+            sprintf("Erreur XML : %s à la ligne %d\n",
+            xml_error_string(xml_get_error_code($xml_parseur)),
+            xml_get_current_line_number($xml_parseur))
+        );
+    }
+    // parcours des lignes du fichier (flux de données):
+    while(!feof($fp)){
+        $page =fgets($fp , 4096);
+        $location = eregi("<location>(.*)</location>", $page, $reg);
+
+while (list(, $value) = each($reg)) {
+    echo "Valeur : $value<br />\n";
 }
-/*
-	echo "<br /> nous nous trouvons pas ce cas de figure <br />";
-	
-	
-}	}
-*/
+        if($reg == $currentVideo){
+            echo "<br /> on se retrouve dans le if(regs == _POST['title'])<br />";
 
- echo " <br/>pas de title <br />";
-
-
-/*
-
-foreach($_POST as $key=>$val){
-	  if ($key == 'title'){	
-		  	echo "<br /> on se retrouve dans le cas de figure ou on a le titre dans le xml <br />";*/
+        }
+    }
+    // fermeture du fichier
+    fclose($fp);
+     */
+}
 ?>
